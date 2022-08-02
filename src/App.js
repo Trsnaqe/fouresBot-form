f0832c36b68c32075866a79ea6778f5e91c79aa8
trsnaqe <trsnaqe@pop-os.localdomain>
2022-08-02 16:38:34 +0300

    Deletion

--- src/App.js
+++ src/App.js
@@ -4,21 +4,25 @@ const axios=require('axios')
 export const AddKey= ()=> {
 
     const [keys,setKeys]=useState()
-    const [games,setGames]=useState()
+    const [games,setGames]=useState('')
     const [click,setClick]=useState(false)
+    const [refresh,setRefresh]=useState(false)
     useEffect(() => {
       axios.get("http://localhost:6006/api/steamKey").then(response=>setGames(response.data.map(element=>element.key) ))
-      
-    }, [click]);
+      GameList=games!=''?games.map((game) =>
+      <li>{game} <button id={game}  key={game} onClick={() => deleteGame(game)}>delete</button></li>
+    ):"no key in db or server is offline"
+    }, [click,refresh]);
     const keyUpdate=(event)=>{ // Dealing with name field changes to update our state
         setKeys(event.target.value)
     }
-    const handleSubmit=()=> { // Once the form has been submitted, this function will post to the backend
+    const handleSubmit=(event)=> { // Once the form has been submitted, this function will post to the backend
+      event.preventDefault()
       axios.post('http://localhost:6006/api/steamKey', {
       keys:keys
       })
       .then(function (response) {
-        alert(response)
+        alert(`Successfully added the game!`)
       })
       .catch(function (error) {
         console.log(error);
@@ -28,8 +32,23 @@ export const AddKey= ()=> {
     const handleClick=()=>{
       setClick(!click)
     }
-    let GameList=games?games.map((game) =>
-    <li>{game}</li>
+    const handleRefresh=()=>{
+      setRefresh(!refresh)
+    }
+    function deleteGame(gameKey){
+      setGames((items) => items.filter((element) => element !== gameKey));
+      axios.post(`http://localhost:6006/api/steamKey/delete/${gameKey}`, {
+        })
+        .then(function (response) {
+          console.log('successfully deleted the game')
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
+    
+    let GameList=games!=''?games.map((game) =>
+    <li>{game} <button id={game}  key={game} onClick={() => deleteGame(game)}>delete</button></li>
   ):"no key in db or server is offline"
 
     return (
@@ -40,11 +59,9 @@ export const AddKey= ()=> {
                 <button type="submit"> Submit</button>
             </form>
             <button onClick={handleClick}>Show all games!</button>
-
-            {click&&
-            <ul>{GameList}</ul>
-            }
-    
+            <button onClick={handleRefresh}>Refresh the Game List!</button>
+          {click&&<ul>{GameList}</ul>}
+      
 
         </div>
     )
